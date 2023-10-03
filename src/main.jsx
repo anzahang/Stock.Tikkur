ab3eab19bbb5e40d4fc514f5feba4f66bd4a532c
Andrew Zhang <64294966+anzahang@users.noreply.github.com>
2023-10-03 00:39:02 -0400

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Import the App component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root') // Replace 'root' with the ID of the HTML element where you want to render your React app
+);
