87e862846a56b7207899ee59a3313a008c3c7b54
Andrew Zhang <64294966+anzahang@users.noreply.github.com>
2023-10-04 12:16:47 -0400

    Update server.py

--- django-server/server.py
+++ django-server/server.py
@@ -2,6 +2,11 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+from wsimple.api import Wsimple
+import re
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+import base64
 
 @csrf_exempt  # Disable CSRF protection for simplicity in this example
 def receive_data(request):
@@ -10,7 +15,34 @@ def receive_data(request):
         email = data.get('email')
         password = data.get('password')
 
-        # Do something with email and password variables
-        print(f'Email: {email}, Password: {password}')
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
-        return JsonResponse({'message': 'Data received successfully'})
+# Path to your OAuth2 credentials JSON file
+CREDENTIALS_FILE = 'path/to/your/credentials.json'
+
+# Regular expression pattern to match OTP (6 digits)
+OTP_PATTERN = r'\b\d{6}\b'
+
+otpFinal = ""
+def get_latest_otp_from_email(service):
+    # Search for emails with the specified subject, sorted by date in descending order (latest first)
+    results = service.users().messages().list(userId='me', q='subject:	Wealthsimple verification code', orderBy='date desc').execute()
+    messages = results.get('messages', [])
+
+    if messages:
+        latest_message = messages[0]  # Get the latest email
+        msg = service.users().messages().get(userId='me', id=latest_message['id'], format='raw').execute()
+        msg_raw = base64.urlsafe_b64decode(msg['raw'].encode('ASCII')).decode('utf-8')
+
+        # Use regular expression to find OTP in the email content
+        otp_matches = re.findall(OTP_PATTERN, msg_raw)
+
+        if otp_matches:
+            otpFinal = otp_matches[0]  # Return the first OTP found (if any)
+
+
+
+
+def WS_Authentication(email,password,otp):
+    pass
+
